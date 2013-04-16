60a0616fa87eee5b956ad71e8a3d84f70ac91371
hanqing <tsinghan07@gmail.com>
2013-04-16 18:45:30 +0800

    init project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tomato::Application.config.secret_token = 'dfc8c87a1ebf924ee212abcd624ef4878b6362d0778a71f935497c5850de7863f5ec6b0e55ff410c2180591c36619aeb3d17b4e5d2d82cd36122814641650f89'
